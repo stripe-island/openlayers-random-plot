70c5f8ac689202e2df79374c0c07e0795710099d
しましまer <109348950+stripe-island@users.noreply.github.com>
2022-11-08 00:42:26 +0900

    表示領域内にランダムに点を表示する処理を実装

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,94 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
+
+import Map from 'ol/Map';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource, OSM } from 'ol/source';
+import { Style, Circle, Fill } from 'ol/style';
+import View from 'ol/View';
+import { fromLonLat, transformExtent } from 'ol/proj';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    onMounted(() => {
+      const source = new VectorSource();
+      const map = new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM()
+          }),
+          new VectorLayer({
+            source: source,
+            style: new Style({
+              image: new Circle({
+                radius: 5.0,
+                fill: new Fill({
+                  color: 'rgba(0, 0, 255, 0.5)'
+                })
+              })
+            })
+          })
+        ],
+        target: 'map',
+        view: new View({
+          center: fromLonLat([136.89826501782872, 35.17743968529702]),
+          zoom: 15.0,
+          minZoom: 5.0,
+          maxZoom: 20.0
+        })
+      });
+
+      let intervalID: number;
+
+      map.addEventListener('movestart', () => {
+        clearInterval(intervalID);
+      });
+
+      map.addEventListener('moveend', () => {
+        intervalID = setInterval(() => {
+          const extent = transformExtent(map.getView().calculateExtent(map.getSize()), 'EPSG:3857', 'EPSG:4326');
+
+          source.addFeature(new Feature({
+            geometry: new Point(fromLonLat([
+              getRandom(extent[0], extent[2]),
+              getRandom(extent[1], extent[3])
+            ]))
+          }));
+        }, 1000);
+      });
+    });
+
+    function getRandom(min: number, max: number): number {
+      return Math.random() * (max - min) + min;
+    }
+  }
 });
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  height: 100%;
+}
+
+#map {
+  height: 100%;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'ol/ol.css'
+
 createApp(App).mount('#app')
